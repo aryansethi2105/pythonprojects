83e71192ddb8fae094610edf58b3ac3feb7b0dbe
aryansethi2105 <aryansethi427@gmail.com>
2026-02-24 22:05:08 +0800

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,6 +5,9 @@ st.title("Password Generator")
 st.header("Password Generator", text_alignment = "center")
 
 with st.form(key = "password_generator", clear_on_submit = True):
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
   nr_letters = st.number_input(label = "Enter the number of letters you would like to include in your password", min_value = 1)
   nr_numbers = st.number_input(label = "Enter the number of numbers you would like to include in your password", min_value = 1)
   nr_ symbols = st.number_input(label = "Enter the number of symbols you would like to include in your password", min_value = 1)
